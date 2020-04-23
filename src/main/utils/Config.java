fabaa06039a89dbb81bbb94f4075345ac5c75105
wowhhh <1792195840@qq.com>
2020-04-23 23:19:45 +0800

    for server

--- src/main/utils/Config.java
+++ src/main/utils/Config.java
@@ -1,9 +1,14 @@
 package main.utils;
 
 public class Config {
+/*  自己机器
     public  static  String reposPath = "E:\\gradle\\repo\\";
-    public static String Drive = "E:"; //D:
-    public static String repoCachePath = "E:\\gradle\\cache\\caches\\modules-2\\files-2.1";
+    public static String Drive = "E:";
+    public static String repoCachePath = "E:\\gradle\\cache\\caches\\modules-2\\files-2.1";*/
+    //服务器
+    public  static  String reposPath = "D:\\gradle\\repo\\";
+    public static String Drive = "D:"; //D:
+    public static String repoCachePath = "D:\\gradle\\cache\\caches\\modules-2\\files-2.1";
 
     public  static  String downSuffix = "/archive/master.zip";
     public static String GET = "GET";
